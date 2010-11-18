b41c8b848598a0efd9ea593f653d6c869178df00
Simon Chiu <skhchiu@gmail.com>
2010-11-18 09:01:40 -0500

    adding more scaffolding for more tests

--- test/remote/test_account_remote.rb
+++ test/remote/test_account_remote.rb
@@ -15,29 +15,50 @@ module RubyPsigate
       )
 
       @account = Account.new(
-
+        # Put in info here
       )
 
+      result = @account.register
+      assert result.success?
     end
 
     def test_failure_in_adding_account
-
+      #
+      #
+      
+      result = @account.register
+      assert !result.success?
+    end
+    
+    # Finding an existing account
+    
+    def test_finding_an_account_with_account_id
+      account_id = nil
+      @account = Account.find(account_id)
+      assert @account
     end
 
     # Delete account
 
     def test_successfully_deleting_account
 
+
+      response = @account.delete
+      assert response.success?
     end
 
     def test_failure_in_deleting_account
 
+
+      response = @account.delete
+      assert !response.success?
     end
 
     # Update account
 
     def test_successfully_updating_account
 
+      response = @account.update
     end
 
     def test_failure_in_updating_account
